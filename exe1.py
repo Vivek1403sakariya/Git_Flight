ca6103481e69dab8124bce0a95d88060845fffb3
Vivek1403sakariya <vivek1403sakariya@gmail.com>
2025-04-29 12:09:02 +0200

    exc1

---
+++ exe1.py
@@ -0,0 +1,8 @@
+print("Hello world")
+print("hello agian")
+print("i like tzping this.")
+print("this is fun")
+print('yay printing')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print("hi #task ")  #comma
